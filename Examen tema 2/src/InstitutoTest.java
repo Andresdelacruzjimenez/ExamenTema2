3598dd5b1184c4b1a9afb4c3e197bc6e45705015
Andrés <andres.saludambiental@gmail.com>
2021-02-08 18:48:46 +0100

    Pruebas finalizadas

--- Examen tema 2/src/InstitutoTest.java
+++ Examen tema 2/src/InstitutoTest.java
@@ -16,6 +16,7 @@ class InstitutoTest {
 	void testGetLocalidad() {
 		
 		assertEquals("Ávila",ejemplo.getLocalidad());
+		assertNotEquals("Salamanca", ejemplo.getLocalidad());
 		
 	}
 
@@ -25,13 +26,14 @@ class InstitutoTest {
 		Instituto ejemplo1=new Instituto("Ávila",40,true,"José Luis",jefe);
 		ejemplo1.setLocalidad("Burgos");
 		assertEquals("Burgos", ejemplo1.getLocalidad());
-	
+		assertNotEquals("León", ejemplo1.getLocalidad());
 	}
 
 	@Test
 	void testGetClases() {
 		
 		assertEquals(40, ejemplo.getClases());
+		assertNotEquals(50, ejemplo.getClases());
 	}
 
 	@Test
@@ -40,11 +42,13 @@ class InstitutoTest {
 		Instituto ejemplo1=new Instituto("Ávila",40,true,"José Luis",jefe);
 		ejemplo1.setClases(63);
 		assertEquals(63, ejemplo1.getClases());
+		assertNotEquals(80, ejemplo1.getClases());
 	}
 
 	@Test
 	void testIsPublico() {
 		assertEquals(true, ejemplo.isPublico());
+		assertNotEquals(false, ejemplo.isPublico());
 	}
 
 	@Test
@@ -53,12 +57,14 @@ class InstitutoTest {
 		Instituto ejemplo1=new Instituto("Ávila",40,true,"José Luis",jefe);
 		ejemplo1.setPublico(false);
 		assertEquals(false, ejemplo1.isPublico());
+		assertNotEquals(true, ejemplo1.isPublico());
 	}
 
 	@Test
 	void testGetDirector() {
 		
 		assertEquals("José Luis", ejemplo.getDirector());
+		assertNotEquals("Luis José", ejemplo.getDirector());
 	}
 
 	@Test
@@ -67,12 +73,14 @@ class InstitutoTest {
 		Instituto ejemplo1=new Instituto("Ávila",40,true,"José Luis",jefe);
 		ejemplo1.setDirector("Pepe Luis");
 		assertEquals("Pepe Luis", ejemplo1.getDirector());
+		assertNotEquals("Jose Luis", ejemplo1.getLocalidad());
 	}
 
 	@Test
 	void testGetJefe() {
-		
+		String[]jefe2= {"Laura, Ana, Sofía"};
 		assertEquals(jefe, ejemplo.getJefe());
+		assertNotEquals(jefe2, ejemplo.getJefe());
 	}
 
 	@Test
@@ -82,11 +90,14 @@ class InstitutoTest {
 		String[]jefe2= {"Laura, Ana, Sofía"};
 		ejemplo1.setJefe(jefe2);
 		assertEquals(jefe2, ejemplo1.getJefe());
+		assertNotEquals(jefe, ejemplo1.getJefe());
 	}
 
 	@Test
 	void testToString() {
 		assertEquals("Ávila, 40 clases,true, Director: José Luis.", ejemplo.toString());
+		assertNotEquals("Ávila,  40 clases,true, Director: José Luis", ejemplo.toString());
+		
 		
 	}
 
